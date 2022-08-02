e10b0a64eb67ae2849187c25634211b4553653c7
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-08-02 13:47:31 -0400

    Update simd_arith.jl

--- src/simd_arith.jl
+++ src/simd_arith.jl
@@ -1,1 +1,15 @@
+function add_dddd_dd(xhi::Vec{N,T}, xlo::Vec{N,T}, yhi::Vec{N,T}, ylo::Vec{N,T}) where {N,T}
+  s, e = two_sum(xhi, yhi)
+  w = xlo + ylo
+  e = e + w
+  two_hilo_sum(s, e)
+end
 
+function add_dddd_dd(xhilo::Vec{N,T}, yhilo::Vec{N,T}) where {N,T}
+  xhi, xlo = xhilo
+  s, e = two_sum(xhilo, yhi)
+  w = xlo + ylo
+  e = e + w
+  rhi, rlo = two_hilo_sum(s, e)
+  rhi, rlo
+end
