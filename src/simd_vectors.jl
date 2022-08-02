72bd829f6e58efc7803e50f1d5073bc19a046b42
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-08-02 10:33:42 -0400

    Update simd_vectors.jl

--- src/simd_vectors.jl
+++ src/simd_vectors.jl
@@ -1,4 +1,8 @@
 simdvec(x::NTuple{N,T}) where {N, T<:SIMDNUMS} = SIMD.Vec{N,T}(x)
 simdvec(x::AbstractVector{T}) where {T<:SIMDNUMS} = simdvec(Tuple(x))
 
+simdvec(x::NTuple{N1,T}, y::NTuple{N2,T}) where {N1, N2, T<:SIMDNUMS} = SIMD.Vec{N1+N2,T}((x..., y...))
+simdvec(x::Vec{N1,T}, y::Vec{N2,T}) where {N1, N2, T<:SIMDNUMS} = SIMD.Vec{N1+N2,T}((x.data..., y.data...))      
+simdvec(x::AbstractVector{T}, y::AbstractVector{T}) where {T<:SIMDNUMS} = simdvec(Tuple(x), Tuple(y))
       
+value(x::Vec{N,T}) where {N,T} = map(z->z.value, x.data)
