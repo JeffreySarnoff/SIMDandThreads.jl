caa7e88dfa4556294a7291252fa77c19f337382f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-08-02 09:50:25 -0400

    Update SIMDandThreads.jl

--- src/SIMDandThreads.jl
+++ src/SIMDandThreads.jl
@@ -7,12 +7,16 @@ import ErrorfreeArithmetic as EFA
 
 const SIMDNUMS = Union{UInt32, UInt64, Int32, Int64, Float32, Float64} 
 
+include("errorfree.jl")
+
 include("simd_vectors.jl")
 include("simd_errorfree.jl")
 include("simd_arith.jl")
 include("simd_fma.jl")
 
-include("errorfree.jl")
+include("simd_ddprearith.jl")
+include("simd_ddarith.jl")
+
 
 
 end  # SIMDandThreads
