4e93bb1cfe7aa2435a7bca120346ce551a4e535f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-08-02 07:08:18 -0400

    Update simd_vectors.jl

--- src/simd_vectors.jl
+++ src/simd_vectors.jl
@@ -1,5 +1,4 @@
-simdvec(x::NTuple{4,T}) where {T<:SIMDNUMS} = SIMD.Vec{4,T}(x)
-simdvec(x::NTuple{8,T}) where {T<:SIMDNUMS} = SIMD.Vec{8,T}(x)
-
+simdvec(x::NTuple{N,T}) where {N, T<:SIMDNUMS} = SIMD.Vec{N,T}(x)
 simdvec(x::AbstractVector{T}) where {T<:SIMDNUMS} = simdvec(Tuple(x))
+
       
