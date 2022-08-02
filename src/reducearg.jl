06827c8989421bb27984423496fcd2f1f3b7d9c3
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-08-02 16:58:51 -0400

    Update reducearg.jl

--- src/reducearg.jl
+++ src/reducearg.jl
@@ -19,7 +19,7 @@ function approx_reducedlnxgte1(x)                  # x into F + f
     J = trunc(Int,floor(128* (signif-1)))   # J is a value in the set {0,1,2,..,127}
     F = 1 + J//128                          # F is a dicrete variable = 1 + {0,1,..,127}//128  in [1, 255/128]  
     f = signif - F                          # f is a continuous variable in [0, 1/128)
-    x1 = f // F                             # reduced input x1 is in [0, 1/128)
+    x1 = f / F                             # reduced input x1 is in [0, 1/128)
     y1 = lnxgfun(x1)                        # g(x1) = log((1+x)/(1-x)) approximates log(1 + x1)
     y = y1 + log(F) + expon * log(2)        # compensate result
     return y
