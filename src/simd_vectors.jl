28c8d0fe0a21a5d99fd9d67d697e06f0b23c792d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-08-03 00:34:24 -0400

    Update simd_vectors.jl

--- src/simd_vectors.jl
+++ src/simd_vectors.jl
@@ -12,9 +12,9 @@ simdvec(x::Vec{N1,T}, y::Vec{N2,T}, z::Vec{N3,T}) where {N1,N2,N3,T} =
 simdvec(x::AbstractVector{T}, y::AbstractVector{T}, z::AbstractVector{T}) where {T} = 
     simdvec(Tuple(x), Tuple(y), Tuple(z))
 
-simdvec(x::NTuple{N1,T}, y::NTuple{N2,T}, z::NTuple{N3,T}, z::NTuple{N4,T}) where {N1,N2,N3,N4,T} =
+simdvec(x::NTuple{N1,T}, y::NTuple{N2,T}, z::NTuple{N3,T}, w::NTuple{N4,T}) where {N1,N2,N3,N4,T} =
     SIMD.Vec{N1+N2+N3+N4,T}((x..., y..., z..., w...))
-simdvec(x::Vec{N1,T}, y::Vec{N2,T}, z::Vec{N3,T}, z::Vec{N4,T}) where {N1,N2,N3,N4,T} = 
+simdvec(x::Vec{N1,T}, y::Vec{N2,T}, z::Vec{N3,T}, w::Vec{N4,T}) where {N1,N2,N3,N4,T} = 
     SIMD.Vec{N1+N2+N3+N4,T}((x.data..., y.data..., z.data..., w.data...))      
 simdvec(x::AbstractVector{T}, y::AbstractVector{T}, z::AbstractVector{T}, w::AbstractVector{T}) where {T} = 
     simdvec(Tuple(x), Tuple(y), Tuple(z), Tuple(w))
