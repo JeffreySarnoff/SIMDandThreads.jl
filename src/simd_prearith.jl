0ffeb2c699ff35ce9b1482012197df7ab51736d6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-08-03 02:41:17 -0400

    Update simd_prearith.jl

--- src/simd_prearith.jl
+++ src/simd_prearith.jl
@@ -1,1 +1,37 @@
+# from Chris Elrod
 
+function findminmax(f::F, x) where {F}
+   T = Base.promote_eltype(x)
+   minind = 0
+   maxind = 0
+   minval = typemax(T)
+   maxval = typemin(T)
+   @inbounds for i = eachindex(x)
+       fx = f(x[i])
+       gt = fx > maxval
+       lt = fx < minval
+       maxval = gt ? fx : maxval
+       minval = lt ? fx : minval
+       maxind = gt ?  i : maxind
+       minind = lt ?  i : minind
+   end
+   minind, maxind
+end
+
+function findminmax_turbo(f::F, x) where {F}
+    T = Base.promote_eltype(x)
+    minind = 0
+    maxind = 0
+    minval = typemax(T)
+    maxval = typemin(T)
+    @turbo for i = eachindex(x)
+       fx = f(x[i])
+       gt = fx > maxval
+       lt = fx < minval
+       maxval = gt ? fx : maxval
+       minval = lt ? fx : minval
+       maxind = gt ?  i : maxind
+       minind = lt ?  i : minind
+    end
+    minind, maxind
+end
